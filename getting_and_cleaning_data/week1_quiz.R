312feba04d7147a4282271a339375eb1096abde5
christopher-c-aud <christopher.c.aud@gmail.com>
2018-02-28 07:02:28 -0500

    updated wee1_quiz and added data files

--- getting_and_cleaning_data/week1_quiz.R
+++ getting_and_cleaning_data/week1_quiz.R
@@ -2,26 +2,26 @@
 ## Author: Chris Aud
 
 ## setwd
-setwd("/nfs/analysis/analysis/kroger/cust_driven_supply_chain/ca")
+setwd("C:/Users/Chris/Desktop/datasciencecoursera/getting_and_cleaning_data")
 
-## create directory for quiz
-dir.create(paste0(getwd(),"/coursera"))
+## create data directory
+dir.create(paste0(getwd(),"/data"))
 
 ##q1
+setwd("data")
 
 ## create empty csv file
-file.create(paste0(getwd(),"/coursera/dlfile.csv"))
-setwd("coursera")
+file.create(paste0(getwd(),"/quiz1_q1file.csv.csv"))
 
 ## check that empty file exists
 list.files()
 
 ## download data into empty file
 download.file(url = "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv", 
-              destfile = paste0(getwd(),"/dlfile.csv"))
+              destfile = paste0(getwd(),"/quiz1_q1file.csv"))
 
 ## read csv into r
-q1data <- read.csv("dlfile.csv")
+q1data <- read.csv("quiz1_q1file.csv")
 
 ## properties with greater than 1 mil value
 sum(q1data[["VAL"]]==24, na.rm = TRUE)
@@ -29,17 +29,19 @@ sum(q1data[["VAL"]]==24, na.rm = TRUE)
 ## q3
 
 ## create empty xlsx file
-file.create(paste0(getwd(),"/q3file.xlsx"))
+file.create(paste0(getwd(),"/quiz1_q3file.xlsx"))
 list.files()
+
 ## download data into empty file
 download.file(url = "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx", 
-              destfile = paste0(getwd(),"/q3file.xlsx"))
+              destfile = paste0(getwd(),"/quiz1_q3file.xlsx"),
+              mode = 'wb')
 
 ## read into r
 ## load excel library
 library(xlsx)
 
-dat <- read.xlsx("q3file.xlsx", sheetIndex = 1, rowIndex = 18:23, colIndex = 7:15)
+dat <- read.xlsx("quiz1_q3file.xlsx", sheetIndex = 1, rowIndex = 18:23, colIndex = 7:15)
 sum(dat$Zip*dat$Ext,na.rm=T)
 
 ## q4
@@ -58,23 +60,27 @@ zipcodes <- xpathSApply(rootNode,"//zipcode",xmlValue)
 sum(zipcodes == 21231)
 
 ## q5
+## install data.table package
+install.packages("data.table")
+library(data.table)
+
 ## create empty csv file
-file.create(paste0(getwd(),"q5file.csv"))
+file.create(paste0(getwd(),"quiz1_q5file.csv"))
 
 ## check that empty file exists
 list.files()
 
 ## download data into empty file
 download.file(url = "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv", 
-              destfile = paste0(getwd(),"/q5file.csv"))
+              destfile = paste0(getwd(),"/quiz1_q5file.csv"))
 
 ## read csv into r
-DT <- fread("q5file.csv")
+DT <- fread("quiz1_q5file.csv")
 
 ## check quickest method
-system.time({mean(DT[DT$SEX==1,]$pwgtp15); mean(DT[DT$SEX==2,]$pwgtp15)})
-system.time({sapply(split(DT$pwgtp15,DT$SEX),mean)})
-system.time({DT[,mean(pwgtp15),by=SEX]})
-system.time({tapply(DT$pwgtp15,DT$SEX,mean)})
-system.time({rowMeans(DT)[DT$SEX==1]; rowMeans(DT)[DT$SEX==2]})
-system.time({mean(DT$pwgtp15,by=DT$SEX)})
+system.time({mean(DT[DT$SEX==1,]$pwgtp15); mean(DT[DT$SEX==2,]$pwgtp15)}) ## .04
+system.time({sapply(split(DT$pwgtp15,DT$SEX),mean)}) ## not DT 
+system.time({DT[,mean(pwgtp15),by=SEX]}) ## .02
+system.time({tapply(DT$pwgtp15,DT$SEX,mean)}) ## 0
+system.time({rowMeans(DT)[DT$SEX==1]; rowMeans(DT)[DT$SEX==2]}) ## error
+system.time({mean(DT$pwgtp15,by=DT$SEX)}) ## 0
